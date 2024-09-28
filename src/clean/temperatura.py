f369b68348d70076b84dd6bbad7971fd90fb1b1e
Brunikito <brunoluiszrosa@gmail.com>
2024-09-28 15:47:30 -0300

    Tratada a base de temperaturas e um gráfico gerado a partir dela

---
+++ src/clean/temperatura.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+import os
+
+def preprocessamento_temperatura(path):
+    # Unindo as duas partes da tabela
+    df1 = pd.read_parquet(os.path.join(path, "temperatura_mes_a_mes1.parquet"))
+    df2 = pd.read_parquet(os.path.join(path, "temperatura_mes_a_mes2.parquet"))
+    df = pd.concat((df1, df2), axis=0)
+
+    # Formatando valores -99.99 para NaN
+    df.replace(-99.99, np.nan, inplace=True)
+
+    # Lista de meses
+    months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+    # Preencher NaNs com a média dos meses anterior e seguinte
+    for i in range(1, len(months)-1):
+        df[months[i]] = df[months[i]].fillna((df[months[(i-1)]]+df[months[(i+1)]])/2)
+
+    # Obtendo uma coluna om o Station_id reduzido
+    df['ID'] = df['Station_ID'].str[:2]
+
+    # Lendo o arquivo com a conversão de ID para o nome
+    # do país e mapeando no DataFrame original
+    with open(os.path.join(path_data, "Conversão Station_id para Pais.txt"), 'r') as file:
+        mapping = {}
+        for line in file:
+            parts = line.strip().split(' ', 1)
+            if len(parts) == 2:
+                code, country = parts
+                mapping[code] = country
+
+    df['country_name'] = df['ID'].map(mapping)
+
+    # Criando uma coluna com a média anual da temperatura
+    df['media_anual'] = df[months].mean(axis=1)
+
+    # Pegando apenas os anos a partir de 1900
+    df_1900_atual = df[df['Year']>1900]
+
+    # Agrupando as estações por país e por ano
+    df_grouped = df_1900_atual.groupby(['Year', 'country_name'], as_index=False)['media_anual'].mean()
+    return df_grouped
---
+++ src/graphs/grafico_temperatura.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+import altair as alt
+
+# Desabilitando o máximo de linhas
+alt.data_transformers.disable_max_rows()
+
+# Importando o modulo para limpeza
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../clean")))
+import temperatura
+
+# Diretório da tabela a ser tratada
+path_data = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../data/brutos")
+
+# Obtendo a tabela tratada
+df = temperatura.preprocessamento_temperatura(path_data)
+
+# Criando uma tabela com a média mundial
+df_world = df.groupby(['Year'], as_index=False)['media_anual'].mean()
+df_world['country_name'] = 'World'
+
+## Fazendo o gráfico
+
+# Países
+Countries = alt.Chart(df, title="Temperatura ao longo dos anos").mark_line(opacity=0.2).encode(
+    # x, y, color, shape, size
+    x = alt.X('Year:N', axis = alt.Axis(values=[year for year in range(1900, 2021, 10)], title='Ano')),
+    y = alt.Y('media_anual:Q', title='Temperatura média'),
+    color = alt.Color('country_name:N').scale(scheme="warmgreys").legend(None)
+)
+
+# Média global
+World = alt.Chart(df_world).mark_line(opacity=1).encode(
+    x = alt.X('Year:N', axis = alt.Axis(values=[year for year in range(1900, 2021, 10)], title='Ano')),
+    y = alt.Y('media_anual:Q', title='Temperatura média'),
+    color=alt.value('black')
+)
+
+# Combinando ambos
+combined_chart = (Countries + World).properties(
+    width=300  # Ajuste a largura aqui
+)
+
+# Salvando o gráfico
+combined_chart.save('src/graphs/gráfico_temperatura.svg')
