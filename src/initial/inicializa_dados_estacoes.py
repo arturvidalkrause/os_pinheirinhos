6f0f215723963946cd9577eec5dca3e42653d597
Artur <avidalkrause@gmail.com>
2024-10-08 12:38:12 -0300

    .

--- src/initial/inicializa_dados_estacoes.py
+++ src/initial/inicializa_dados_estacoes.py
@@ -8,8 +8,11 @@ path_atual = os.getcwd()
 file_path_read = Path('data/brutos/dados_sobre_estacoes_metereologicas.inv')
 file_path_write = Path('data/tratados/dados_sobre_estacoes_metereologicas.parquet')
 
-# Carregar o arquivo como um arquivo de largura fixa (sem cabeçalho)
-df_uploaded = pd.read_fwf(path_atual / file_path_read, header=None)
+# Definir as larguras das colunas manualmente (usando colspecs)
+colspecs = [(0, 12), (12, 21), (22, 30), (31, 37), (38, 100)]  # Ajustar conforme necessário
+
+# Carregar o arquivo de largura fixa com as larguras de coluna especificadas
+df_uploaded = pd.read_fwf(path_atual / file_path_read, colspecs=colspecs, header=None)
 
 # Definir os nomes das colunas
 df_uploaded.columns = ['Station_ID', 'Latitude', 'Longitude', 'Elevation', 'Location_Name']
